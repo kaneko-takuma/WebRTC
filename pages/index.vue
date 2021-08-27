91dae1a3a7602c26be6cfdfba51760763995ac9a
kaneko-takuma <in-kanekotakuma@team-lab.com>
2021-08-27 18:15:29 +0900

    [Update] etc...

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,19 @@
 <template>
   <div>
-    {{ userName }}
-    <input v-model="userName" type="text">
-    <input v-model="roomName" type="text">
-    <Skyway :userName="userName" :roomName="roomName"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
-import Skyway from '../components/Skyway.vue'
+import { Vue } from 'nuxt-property-decorator';
 
 export default Vue.extend({
-  components: {
-    Skyway
-  },
   data() {
     return {
-      userName: '' as string,
       roomName: '' as string
     }
-  }
+  },
+  mounted() {
+    
+  },
 })
 </script>
---
+++ pages/textchat/_userName.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ userName }}
+    <input v-model="userName" type="text">
+    <input v-model="roomName" type="text">
+    <Skyway :userName="userName" :roomName="roomName"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'nuxt-property-decorator'; 
+import Skyway from '../../components/Skyway.vue'
+
+export default Vue.extend({
+  components: {
+    Skyway
+  },
+  data() {
+    return {
+      userName: '' as string | (string | null)[],
+      roomName: '' as string | (string | null)[]
+    }
+  },
+  mounted() {
+    this.userName = this.$route.query.name
+    this.roomName = this.$route.query.room
+  },
+})
+</script>
